e97a0c8cd022e7d027e61306a7ccc40278665b17
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2025-10-23 04:47:56 +0000

    paperless-ngx: update 2.19.1 bottle.

--- Formula/paperless-ngx.rb
+++ Formula/paperless-ngx.rb
@@ -9,8 +9,8 @@ class PaperlessNgx < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/ingmarstein/paperless-ngx"
-    sha256 arm64_tahoe:  "cc21ef011909c1062437b41342ea90e95c5453d369073fa294360d8d70d889ee"
-    sha256 x86_64_linux: "4cc38e6554a9e443211777e56e93c6d6787698580f8aa1c6ce539ce94ebed058"
+    sha256 arm64_tahoe:  "69e5cb7c22aa91cc5f8cdeb60f6152d4960f661904f551ea2816bfa04515aa4e"
+    sha256 x86_64_linux: "660d4832030cb0d387d9e338f7da788dbd2ac0a99ddcb91c8bc1093bcd261cf9"
   end
 
   depends_on "angular-cli" => :build
