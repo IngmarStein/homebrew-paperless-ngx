ef4de7258d4383f91a0306b69f76cbcacd9dd41e
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-03-21 12:23:54 +0000

    paperless-ngx 2.20.13

--- Formula/paperless-ngx.rb
+++ Formula/paperless-ngx.rb
@@ -3,8 +3,8 @@ class PaperlessNgx < Formula
 
   desc "Scan, index and archive all your physical documents"
   homepage "https://docs.paperless-ngx.com/"
-  url "https://github.com/paperless-ngx/paperless-ngx/releases/download/v2.20.12/paperless-ngx-v2.20.12.tar.xz"
-  sha256 "c1bad772283a8ce0592a9eed1422ebc9903e726841372c7496f108d88f1b7e8b"
+  url "https://github.com/paperless-ngx/paperless-ngx/releases/download/v2.20.13/paperless-ngx-v2.20.13.tar.xz"
+  sha256 "9bc596c950447cfbb4d5867fcc21608217cfe5af39650aacb07241b36fae134b"
   license "GPL-3.0-or-later"
 
   bottle do
